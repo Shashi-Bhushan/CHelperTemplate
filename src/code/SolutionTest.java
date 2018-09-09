41271e1582f7048538b5438b6d26591e83c212d6
Shashi Bhushan <sbhushan1@outlook.com>
2018-09-09 08:57:13 +0530

    Input Reader Exhausted

--- src/code/SolutionTest.java
+++ src/code/SolutionTest.java
@@ -3,35 +3,15 @@ package code;
 import FastIO.InputReader;
 import FastIO.OutputWriter;
 
+import java.util.Scanner;
+
 public class SolutionTest {
     public void solve(int testNumber, InputReader in, OutputWriter out) {
-        int t = in.nextInt();
-        for (int i = 0; i < t; i++) {
-            int a = in.nextInt();
-            int b = in.nextInt();
-            int n = in.nextInt();
+        int index = 1;
 
-            print(a, b, n, out);
+        while(!in.isExhausted()) {
+            out.println(index + " " + in.readLine());
+            index++;
         }
-    }
-
-    private static void print(int a, int b, int n, OutputWriter out) {
-        for (int index = 0; index < n; index++) {
-            out.print(printInternal(a, b, index) + " ");
-        }
-
-        out.println();
-    }
-
-    private static int printInternal(int a, int b, int index) {
-        int sum = a;
-
-        while (index >= 0) {
-            sum += (Math.pow(2, index) * b);
-
-            index--;
-        }
-
-        return sum;
     }
 }
